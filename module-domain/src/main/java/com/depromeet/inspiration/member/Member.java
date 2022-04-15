6edd6b8438aa409fd20d4bbfaec7d6140c5cedff
jeonghyeong-il <junghyungile@gmail.com>
2022-04-15 19:38:42 +0900

    refactor : 컬럼 수정

--- module-domain/src/main/java/com/depromeet/inspiration/member/Member.java
+++ module-domain/src/main/java/com/depromeet/inspiration/member/Member.java
@@ -19,6 +19,7 @@ public class Member extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
     private Long id;
 
     @Column(nullable = false)
