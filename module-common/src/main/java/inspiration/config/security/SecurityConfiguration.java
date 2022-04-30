40e6c80d11ec5512f432f6ac8b007f48da319709
jeonghyeong-il <junghyungile@gmail.com>
2022-05-01 01:03:31 +0900

    refactor : 시큐리티 설정 수정

--- module-common/src/main/java/inspiration/config/security/SecurityConfiguration.java
+++ module-common/src/main/java/inspiration/config/security/SecurityConfiguration.java
@@ -40,14 +40,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
                 .and()
                 .authorizeRequests()
-                .antMatchers(HttpMethod.POST, "/api/v1/members/**").anonymous()
-                .antMatchers(HttpMethod.GET, "/api/v1/members/**").anonymous()
+                .antMatchers(HttpMethod.POST, "/api/v1/members/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/v1/members/**").permitAll()
                 .antMatchers(HttpMethod.DELETE, "/api/v1/members/**").permitAll()
                 .antMatchers(HttpMethod.PUT, "/api/v1/members/**").permitAll()
 
                 .antMatchers("/v2/api-docs", "/swagger-resources/**",
                         "/swagger-ui.html", "/webjars/**", "/swagger/**", "/api/v1/members/**").permitAll()
-                .anyRequest().authenticated()
 
                 .and()
                 .addFilterBefore(new JwtAuthenticationFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class);
