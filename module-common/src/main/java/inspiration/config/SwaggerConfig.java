47e606433a80085a3d52d98bcd3a38f430931f34
jeonghyeong-il <junghyungile@gmail.com>
2022-05-01 00:23:52 +0900

    refactor : 스웨거 포트 수정

--- module-common/src/main/java/inspiration/config/SwaggerConfig.java
+++ module-common/src/main/java/inspiration/config/SwaggerConfig.java
@@ -26,7 +26,7 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
     @Bean
     public Docket commonApi() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .host("13.125.36.7")
+                .host("13.125.36.7:8080")
                 .apiInfo(this.apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("inspiration.v1"))
