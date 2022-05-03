776eaa1f7fb0c611751c97f6aab7f0f683074f58
jeonghyeong-il <junghyungile@gmail.com>
2022-05-04 01:28:53 +0900

    feature : 리프레시 토큰 재발급 기능 추가

--- module-api/src/main/java/inspiration/auth/AuthService.java
+++ module-api/src/main/java/inspiration/auth/AuthService.java
@@ -4,14 +4,15 @@ import inspiration.auth.request.LoginRequest;
 import inspiration.config.security.JwtProvider;
 import inspiration.enumeration.ExceptionType;
 import inspiration.exception.PostNotFoundException;
+import inspiration.exception.RefreshTokenException;
 import inspiration.member.Member;
 import inspiration.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import javax.servlet.http.HttpServletResponse;
 import javax.transaction.Transactional;
 import java.util.concurrent.TimeUnit;
 
@@ -23,28 +24,52 @@ public class AuthService {
     private final StringRedisTemplate redisTemplate;
     private final PasswordEncoder passwordEncoder;
     private final MemberRepository memberRepository;
+
     private final Long refreshTokenValidMillisecond = 14 * 24 * 60 * 60 * 1000L;
+    private final String refreshTokenKey = "refreshToken : ";
 
     @Transactional
     public void login(LoginRequest request) {
 
-        Member member = existsEmailCheck(request.getEmail());
+        Member member = checkEmail(request.getEmail());
 
         isValidPassword(request.getPassword(), member.getPassword());
 
         refreshTokenSave(member.getId(), jwtProvider.createTokenDto(member.getId(), member.getRoles()));
     }
 
-    private void refreshTokenSave(Long memberId, String refreshToken) {
-        redisTemplate.opsForValue().set("refreshToken : " + memberId, refreshToken, refreshTokenValidMillisecond, TimeUnit.MILLISECONDS);
+    @Transactional
+    public void reissue(String accessToken) {
+        System.out.println(accessToken);
+
+        Authentication authentication = jwtProvider.getAuthentication(accessToken);
+
+        Member member = memberRepository.findById(Long.parseLong(authentication.getName()))
+                .orElseThrow(() -> new PostNotFoundException(ExceptionType.MEMBER_NOT_FOUND.getMessage()));
+
+        String refreshToken = redisTemplate.opsForValue().get(refreshTokenKey + member.getId());
+
+        if (refreshToken == null) {
+            throw new RefreshTokenException();
+        }
+
+        String newCreatedToken = jwtProvider.createTokenDto(member.getId(), member.getRoles());
+
+        refreshTokenSave(member.getId(), newCreatedToken);
     }
 
-    private Member existsEmailCheck(String email) {
-        return memberRepository.findByEmail(email)
-                .orElseThrow(() -> new PostNotFoundException(ExceptionType.EMAIl_NOT_FOUND.getMessage()));
+    private void refreshTokenSave(Long memberId, String refreshToken) {
+
+        redisTemplate.opsForValue().set(refreshTokenKey + memberId, refreshToken, refreshTokenValidMillisecond, TimeUnit.MILLISECONDS);
+    }
+
+    private Member checkEmail(String email) {
+
+        return memberRepository.findByEmail(email).orElseThrow(() -> new PostNotFoundException(ExceptionType.EMAIl_NOT_FOUND.getMessage()));
     }
 
     private void isValidPassword(String requestPassword, String realPassword) {
+
         if (!passwordEncoder.matches(requestPassword, realPassword))
             throw new PostNotFoundException(ExceptionType.VALID_NOT_PASSWORD.getMessage());
     }
