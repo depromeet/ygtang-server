a6f01c473b1253813302616a6133d9e5cf4a41c5
IW-MOON <mooniw02@gmail.com>
2022-05-07 13:45:48 +0900

    feat: 영감, 태그 관련 기능 추가

--- module-domain/src/main/java/inspiration/inspiration/InspirationRepository.java
+++ module-domain/src/main/java/inspiration/inspiration/InspirationRepository.java
@@ -4,11 +4,9 @@ import inspiration.member.Member;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 
 public interface InspirationRepository extends JpaRepository<Inspiration, Long>{
 
-    @Query(value = "select i from Inspiration i")
     Page<Inspiration> findAllByIsDeletedAndMember(boolean isDeleted, Member member, Pageable pageable);
 }
