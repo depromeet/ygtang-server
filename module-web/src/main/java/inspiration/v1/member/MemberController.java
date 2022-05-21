863b53e946855cc6a2022fd304aa46e3e5d95d4d
nature1216 <annekim1216@naver.com>
2022-05-21 21:11:37 +0900

    Feat: getMyInfo
    1. 사용자 정보 조회
    
    Feat #43

--- module-web/src/main/java/inspiration/v1/member/MemberController.java
+++ module-web/src/main/java/inspiration/v1/member/MemberController.java
@@ -1,8 +1,10 @@
 package inspiration.v1.member;
 
+import inspiration.auth.AuthService;
 import inspiration.config.AuthenticationPrincipal;
 import inspiration.member.MemberService;
 import inspiration.member.request.UpdatePasswordRequest;
+import inspiration.member.response.MemberInfoResponse;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -16,11 +18,19 @@ import javax.validation.Valid;
 public class MemberController {
 
     private final MemberService memberService;
+    private final AuthService authService;
 
     @PutMapping("/passwords/reset")
     @ApiOperation(value = "패스워드를 변경.", notes = "패스워드를 변경한다.")
     public void updatePassword(@ApiIgnore @AuthenticationPrincipal Long memberId, @RequestBody @Valid UpdatePasswordRequest request) {
 
         memberService.updatePassword(memberId, request.getConfirmPassword(), request.getPassword());
+    }
+
+    @PostMapping("/info")
+    @ApiOperation(value = "사용자 정보 조회", notes = "사용자 정보를 조회한다.")
+    public MemberInfoResponse getUserInfo() {
+
+        return authService.getUserInfo();
     }
 }
