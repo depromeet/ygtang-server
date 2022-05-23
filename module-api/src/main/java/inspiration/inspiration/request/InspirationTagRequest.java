889e1bee04305fb37cd0de2abb7e882aba40f2d7
IW-MOON <mooniw02@gmail.com>
2022-05-23 20:52:17 +0900

    fix:  영감 태깅 validation 수정

--- module-api/src/main/java/inspiration/inspiration/request/InspirationTagRequest.java
+++ module-api/src/main/java/inspiration/inspiration/request/InspirationTagRequest.java
@@ -6,7 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 
 @Getter
@@ -15,9 +15,9 @@ import javax.validation.constraints.NotBlank;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class InspirationTagRequest {
 
-    @NotBlank(message = "영감 id는 필수 입력 입니다.")
+    @NotNull(message = "영감 id는 필수 입력 입니다.")
     private Long id;
-    @NotBlank(message = "태그 id는 필수 입력 입니다.")
+    @NotNull(message = "태그 id는 필수 입력 입니다.")
     private Long tagId;
 
 }
