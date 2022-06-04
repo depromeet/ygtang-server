920b5877e0d78c040743968221c43488cc981fe5
Hyung1Jung <junghyungile@gmail.com>
2022-06-04 15:15:08 +0900

    메일 링크 수정 (#58)

--- module-domain/src/main/java/inspiration/member/MemberRepository.java
+++ module-domain/src/main/java/inspiration/member/MemberRepository.java
@@ -1,11 +1,14 @@
 package inspiration.member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
+import javax.persistence.LockModeType;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<Member> findByEmail(String email);
 
     boolean existsByNickname(String nickName);
