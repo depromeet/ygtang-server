f591eca54cacaec34d22821229ad7026f1effbf5
Hyung1Jung <junghyungile@gmail.com>
2022-06-04 16:34:12 +0900

    동시성 제어 write -> read (#62)

--- module-domain/src/main/java/inspiration/member/MemberRepository.java
+++ module-domain/src/main/java/inspiration/member/MemberRepository.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Lock(LockModeType.PESSIMISTIC_READ)
     Optional<Member> findById(Long id);
 
     Optional<Member> findByEmail(String email);
