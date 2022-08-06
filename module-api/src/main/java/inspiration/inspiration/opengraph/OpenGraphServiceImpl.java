ddab886dcdbfb0ad630fb13d33f001904c64f6c4
Haeseong Jeon <seong0428@gmail.com>
2022-08-06 18:41:48 +0900

    docs: 주석 추가

--- module-api/src/main/java/inspiration/inspiration/opengraph/OpenGraphServiceImpl.java
+++ module-api/src/main/java/inspiration/inspiration/opengraph/OpenGraphServiceImpl.java
@@ -9,6 +9,7 @@ import java.util.Optional;
 @Service
 public class OpenGraphServiceImpl implements OpenGraphService {
     public Optional<OpenGraphVo> getMetadata(String url) {
+        // FIXME: DI 적용 (모듈 의존성 개선 후 api 모듈에서 bean 생성해야함)
         OgParser ogParser = new OgParser(new YgtangOgMetaElementHtmlParser());
         OpenGraph openGraph = ogParser.getOpenGraphOf(url);
         if (openGraph.getAllProperties().isEmpty()) {
