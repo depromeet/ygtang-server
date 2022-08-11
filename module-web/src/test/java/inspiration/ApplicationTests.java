849fd4741bb04a0357d9a8ca003f358cb38c7a72
Haeseong Jeon <seong0428@gmail.com>
2022-08-12 03:04:29 +0900

    test: 테스트 위한 환경 설정

---
+++ module-web/src/test/java/inspiration/ApplicationTests.java
@@ -0,0 +1,11 @@
+package inspiration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
---
+++ module-web/src/test/java/inspiration/TestRedisConfiguration.java
@@ -0,0 +1,28 @@
+package inspiration;
+
+import inspiration.property.RedisProperties;
+import org.springframework.boot.test.context.TestConfiguration;
+import redis.embedded.RedisServer;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@TestConfiguration
+public class TestRedisConfiguration {
+
+    private final RedisServer redisServer;
+
+    public TestRedisConfiguration(RedisProperties redisProperties) {
+        this.redisServer = new RedisServer(redisProperties.getPort());
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        redisServer.start();
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        redisServer.stop();
+    }
+}
