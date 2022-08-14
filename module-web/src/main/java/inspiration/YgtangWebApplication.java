e8479842effc4385cc4bd15340edf2241949aba7
IW-MOON <mooniw02@gmail.com>
2022-08-14 23:18:55 +0900

    feat: 자동화 배포환경 구축(jar copy, docker open port 추가, JAVA_OPTS 주입)

--- module-web/src/main/java/inspiration/YgtangWebApplication.java
+++ module-web/src/main/java/inspiration/YgtangWebApplication.java
@@ -2,11 +2,8 @@ package inspiration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@ConfigurationPropertiesScan("inspiration.property")
 public class YgtangWebApplication {
     public static void main(String[] args) {
         System.setProperty("spring.config.name", "application,web");
