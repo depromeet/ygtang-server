149c9d6baff28f5ddaacc54a5ff0d9efb828593a
Haeseong Jeon <seong0428@gmail.com>
2022-08-20 02:15:45 +0900

    config: jasypt 자동설정 적용

---
+++ module-domain/src/main/java/inspiration/infrastructure/jaspyt/JasyptConfig.java
@@ -0,0 +1,9 @@
+package inspiration.infrastructure.jaspyt;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.springframework.context.annotation.Configuration;
+
+@EnableEncryptableProperties
+@Configuration
+public class JasyptConfig {
+}
