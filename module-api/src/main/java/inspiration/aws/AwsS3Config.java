485dd468e4a0d0347de8931ac557d1cf25d9709d
전해성 <seong0428@gmail.com>
2022-08-26 01:17:57 +0900

    config: aws s3 client 타임아웃 추가 (#160)
    * config: aws s3 client 타임아웃 추가
    
    * config: 타임아웃 값 외부에서 주입
    
    * test: 테스트 실패 해결

--- module-api/src/main/java/inspiration/aws/AwsS3Config.java
+++ module-api/src/main/java/inspiration/aws/AwsS3Config.java
@@ -1,5 +1,6 @@
 package inspiration.aws;
 
+import com.amazonaws.ClientConfiguration;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AwsS3Config {
+
     @Value("${cloud.aws.credentials.access-key}")
     private String accessKey;
 
@@ -20,12 +22,22 @@ public class AwsS3Config {
     @Value("${cloud.aws.region.static}")
     private String region;
 
+    @Value("${cloud.aws.s3.connection-timeout:1000}")
+    private int connectionTimeout;
+
+    @Value("${cloud.aws.s3.request-timeout:3000}")
+    private int requestTimeout;
+
     @Bean
     public AmazonS3 amazonS3() {
         AWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
+        ClientConfiguration clientConfiguration = new ClientConfiguration();
+        clientConfiguration.setConnectionTimeout(connectionTimeout);
+        clientConfiguration.setRequestTimeout(requestTimeout);
         return AmazonS3ClientBuilder.standard()
                                     .withRegion(region)
                                     .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                                    .withClientConfiguration(clientConfiguration)
                                     .build();
     }
 
