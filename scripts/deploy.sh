706409df0e04d7191bfd47525cd80d4071919735
IW-MOON <mooniw02@gmail.com>
2022-08-28 12:12:20 +0900

    feat: docker build를 위한 AWS CodeBuild 환경 연동 (#155)
    * feat: docker build를 위한 AWS CodeBuild 환경 연동
    
    * refactor: 사용되지 않는 스크립트 제거

---
+++ scripts/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+APP_PATH=/home/ubuntu/app/inspiration
+SCRIPT_PATH=/home/ubuntu/app/inspiration/scripts
+
+cp ${APP_PATH}/deploy/.env ${APP_PATH}/source/.env
+export $(cat ${APP_PATH}/source/.env | grep -v ^# | xargs) >/dev/null
+
+cd ${SCRIPT_PATH}
+
+$SCRIPT_PATH/login_ecr.sh ${AWS_CLI_REGION} ${AWS_CLI_ACCOUNT_ID}
+
+docker stop ${ACTIVE}
+sleep 3
+
+docker-compose -f ${APP_PATH}/source/docker-compose.yml pull
+sleep 1
+docker-compose -f ${APP_PATH}/source/docker-compose.yml up -d
+sleep 10
