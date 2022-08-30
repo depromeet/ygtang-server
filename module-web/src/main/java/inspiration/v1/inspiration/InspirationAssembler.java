9a6a6446e15db6fafa56c7765bc591ee67ef9ff2
Haeseong Jeon <seong0428@gmail.com>
2022-08-31 00:09:40 +0900

    refactor: code smell 제거

--- module-web/src/main/java/inspiration/v1/inspiration/InspirationAssembler.java
+++ module-web/src/main/java/inspiration/v1/inspiration/InspirationAssembler.java
@@ -16,7 +16,6 @@ import org.springframework.stereotype.Component;
 
 import java.util.concurrent.Callable;
 import java.util.concurrent.CompletableFuture;
-import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
