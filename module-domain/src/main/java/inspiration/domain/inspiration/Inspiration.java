3976a57c8805b4d8af4ac18038f0ba18b981439c
Haeseong Jeon <seong0428@gmail.com>
2023-03-12 00:20:43 +0900

    fix: 병렬 처리 로직 개선

--- module-domain/src/main/java/inspiration/domain/inspiration/Inspiration.java
+++ module-domain/src/main/java/inspiration/domain/inspiration/Inspiration.java
@@ -21,7 +21,7 @@ public class Inspiration extends BaseTimeEntity {
     @Column(name = "inspiration_id")
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne
     @JoinColumn(name = "member_id")
     private Member member;
 
