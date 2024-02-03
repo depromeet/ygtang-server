1188160a0ab839abd283416f406ca65e7316a005
sian <chicori3@naver.com>
2024-02-03 17:38:43 +0900

    닉네임 Size 제한 수정

--- module-api/src/main/java/inspiration/domain/member/request/SignUpRequest.java
+++ module-api/src/main/java/inspiration/domain/member/request/SignUpRequest.java
@@ -16,7 +16,7 @@ public class SignUpRequest {
     private String email;
 
     @NotBlank(message = "올바른 닉네임을 입력해주세요.")
-    @Size(min = 4, max = 20, message = "닉네임은 4자 이상 20자 이하로 입력해주세요.")
+    @Size(min = 2, max = 20, message = "닉네임은 2자 이상 20자 이하로 입력해주세요.")
     private String nickName;
 
     @NotBlank(message = "올바른 비밀번호를 입력해주세요.")
