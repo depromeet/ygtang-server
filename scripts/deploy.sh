a1f65cfc13de44eeedf8a25a40cc66e2eba62a32
sian <chicori3@naver.com>
2024-05-11 19:41:11 +0900

    fix deploy sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -15,5 +15,5 @@ sleep 3
 
 docker-compose -f ${APP_PATH}/source/docker-compose.yml pull
 sleep 1
-docker-compose -f ${APP_PATH}/source/docker-compose.yml up
+docker-compose -f ${APP_PATH}/source/docker-compose.yml up -d
 sleep 10
