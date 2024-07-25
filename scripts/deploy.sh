33d19a6cf168c6fbf6d94ec6c608005ebbc28ac2
sian <chicori3@naver.com>
2024-07-25 21:24:37 +0900

    fix: deploy aws (#218)
    * rename bucket name
    
    * fix encrypted properties value
    
    * fix github actions
    
    * fix deploy sh
    
    * fix deploy sh
    
    * fix deploy sh
    
    * fix health check
    
    * deploy sh check log
    
    * fix jdbc url
    
    * fix flyway config
    
    * fix jdbc url
    
    * fix jdbc url
    
    * remove docker compose command
    
    * fix flyway config
    
    * fix jdbc url
    
    * fix jdbc url
    
    * fix deploy sh
    
    * add hikari maximum pool size
    
    * fix s3 bucket source

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,6 +6,7 @@ cp ${APP_PATH}/deploy/.env ${APP_PATH}/source/.env
 export $(cat ${APP_PATH}/source/.env | grep -v ^# | xargs) >/dev/null
 
 cd ${SCRIPT_PATH}
+chmod +x login_ecr.sh
 
 $SCRIPT_PATH/login_ecr.sh ${AWS_CLI_REGION} ${AWS_CLI_ACCOUNT_ID}
 
